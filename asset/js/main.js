f9636e26dd092e2a871219ef34f596aa41e59692
mouad chahid <mouadechahide1@gmail.com>
2023-11-11 14:59:07 +0100

    first commit

---
+++ asset/js/main.js
@@ -0,0 +1,8 @@
+const imageSlide = (Newsrc) => { 
+    const mainImage = document.querySelector(".starBucks") ; 
+    mainImage.src = Newsrc ;
+} 
+const changeCircleBackground = (color) => { 
+const circle = document.querySelector(".circle") ; 
+circle.style.background = color ;
+}
